c42089f5c95dbf98b23ff7afe3b8d8e5e6c5e346
Judy Mwai <juddiemukami@gmail.com>
2022-06-14 22:54:14 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form= document.querySelector('form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    createContent(e.target.new_task_description.value)
+    form.reset()
+
+  })
+
+  function createContent(list){
+    const p= document.createElement('p')
+    p.textContent=`${list} `
+    const btn = document.createElement('button')
+    btn.textContent='x'
+    p.append(btn)
+
+    const element= document.getElementById('tasks')
+  
+    element.append(p)
+    btn.addEventListener('click', deleteList)
+
+    
+
+  }
+ 
+function deleteList(e){
+  e.target.parentNode.remove()
+
+}
+
+
+
 });
